502b50ad203153539be0d771fa57a0e9336185d0
SONG <songxowe@hotmail.com>
2019-04-11 20:35:35 +0800

    add Maven.java

---
+++ newer-git-maven/src/main/java/com/newer/Maven.java
@@ -0,0 +1,11 @@
+package com.newer;
+
+/**
+ * @author SONG
+ * @date 2019/4/11 20:32
+ */
+public class Maven {
+  public static void main(String[] args) {
+    System.out.println("Maven");
+  }
+}
