8d49c34be6d2cf16d74658cf9fd6b2556bc1993e
SONG <songxowe@hotmail.com>
2019-04-11 20:21:03 +0800

    修改 App.java:github git idea

--- newer-git-maven/src/main/java/com/newer/App.java
+++ newer-git-maven/src/main/java/com/newer/App.java
@@ -2,12 +2,10 @@ package com.newer;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+    System.out.println("Hello Github - Git - IDEA");
+  }
 }
